92549d7034b9063681b52adb5646fb15e06c991b
Min Kim <minkimcodes@gmail.com>
2023-10-01 17:47:07 -0400

    Loading animation

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import UserTable from './new/UserTable'
+import Link from 'next/link';
 
 interface Props {
   searchParams: {
@@ -11,7 +12,10 @@ const UsersPage = async ({ searchParams: {sortOrder}}: Props) => {
   return (
     <>
       <h1>Users</h1>
-      <UserTable sortOrder={sortOrder}/>
+      <Link href="/users/new" className="btn">New User</Link>
+      <Suspense fallback={<p>Loading...</p>}>
+        <UserTable sortOrder={sortOrder}/>
+      </Suspense>
     </>
   )
 }
