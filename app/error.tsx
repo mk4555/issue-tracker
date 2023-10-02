0f4b4fa4a42be2c8e794b734e43821bdef5efcab
Min Kim <minkimcodes@gmail.com>
2023-10-02 17:18:07 -0400

    Error handling with reset

--- app/error.tsx
+++ app/error.tsx
@@ -1,9 +1,18 @@
 'use client';
 import React from 'react'
 
-const ErrorPage = () => {
+interface Props {
+  error: Error;
+  reset: () => void;
+}
+
+const ErrorPage = ({ error, reset }: Props) => {
+  console.log("Error ", error)
   return (
+    <>
     <div>An unexpected error has occured.</div>
+    <button className='btn' onClick={() => reset()}>Retry</button>
+    </>
   )
 }
 
